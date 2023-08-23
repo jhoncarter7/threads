0205d261a90592acbae0149a3d126cfa50b413a3
jhoncarter7 <rainikhil044@gmail.com>
2023-08-23 23:43:27 +0530

    [feat]: created navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,11 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 import '../globals.css'
-export const metadata: Metadata = {
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
+export const metadata = {
   title: "Threads",
   description: "A next.js 13 meta threads",
 }
@@ -18,7 +22,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+          <LeftSidebar/>
+          <section className='main-container'>
+            <div className='w-full max-w-4xl'>
+            {children}
+            </div>
+          </section>
+          <RightSidebar/>
+          </main>
+          
+          <Bottombar/>
+
+          
+         </body>
       </html>
     </ClerkProvider>
     )
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,8 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <h1>sdjbcasjb</h1>
-      <UserButton afterSignOutUrl="/"/>
+      <h1>Home</h1>
+      {/* <UserButton afterSignOutUrl="/"/> */}
     </div>
   )
 }
