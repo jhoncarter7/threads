c1de570e378db2664039151163abb09901bd0a0e
jhoncarter7 <rainikhil044@gmail.com>
2023-08-25 23:55:49 +0530

    [feat]: added signout

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,6 +5,7 @@ import {sidebarLinks} from '@/constants/index'
 import Link from 'next/link'
 import { Image } from 'next/dist/client/image-component'
 import {usePathname, useRouter} from 'next/navigation'
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
 const LeftSidebar = () => {
   const router = useRouter()
   const pathname = usePathname()
@@ -16,12 +17,27 @@ const LeftSidebar = () => {
         return(
           <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
             <Image src={link.imgURL} alt={link.label} width={24} height={24}/>
-          <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+          <p className='text-light-1 max-lg:hidden '>{link.label}</p>
           </Link>
         )
           })
         }
         
+      </div>
+      <div className="mt-10 px-6">
+      <SignedIn>
+            <SignOutButton signOutCallback={()=> router.push('/sign-in')}>
+              <div className="flex cursor-pointer gap-4 p-4">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+                <p className='text-light-2  max-lg:hidden'>Logout</p>
+              </div>
+            </SignOutButton>
+          </SignedIn>
       </div>
     </section>
   )
