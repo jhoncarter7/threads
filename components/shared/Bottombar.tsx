c9192f134a07c1386b780515269ced1633b79e62
jhoncarter7 <rainikhil044@gmail.com>
2023-08-26 23:26:05 +0530

    [feat]: created footer

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+'use client'
+import { sidebarLinks } from "@/constants";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
 
 const Bottombar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
-    <div>Bottombar</div>
-  )
-}
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          // pathname.includes(link.route) is checking if the current URL path (pathname) includes the route of the link (link.route).
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link href={link.route} className={`bottombar_link  ${isActive && "bg-primary-500"}`}>
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className=" text-subtle-medium text-light-1 max-sm:hidden"  >{link.label.split(/\s+/)[0]}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Bottombar
+export default Bottombar;
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,46 +1,56 @@
-"use client"
+"use client";
 
-import React from 'react'
-import {sidebarLinks} from '@/constants/index'
-import Link from 'next/link'
-import { Image } from 'next/dist/client/image-component'
-import {usePathname, useRouter} from 'next/navigation'
+import React from "react";
+import { sidebarLinks } from "@/constants/index";
+import Link from "next/link";
+import { Image } from "next/dist/client/image-component";
+import { usePathname, useRouter } from "next/navigation";
 import { SignedIn, SignOutButton } from "@clerk/nextjs";
 const LeftSidebar = () => {
-  const router = useRouter()
-  const pathname = usePathname()
+  const router = useRouter();
+  const pathname = usePathname();
   return (
-    <section className='custome-scrollbar  leftsidebar'>
-      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+    <section className="custome-scrollbar  leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
-          const isActive= (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
-        return(
-          <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
-            <Image src={link.imgURL} alt={link.label} width={24} height={24}/>
-          <p className='text-light-1 max-lg:hidden '>{link.label}</p>
-          </Link>
-        )
-          })
-        }
-        
+          //  pathname.includes(link.route) is checking if the current URL path (pathname) includes the route of the link (link.route).
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden ">{link.label}</p>
+            </Link>
+          );
+        })}
       </div>
       <div className="mt-10 px-6">
-      <SignedIn>
-            <SignOutButton signOutCallback={()=> router.push('/sign-in')}>
-              <div className="flex cursor-pointer gap-4 p-4">
-                <Image
-                  src="/assets/logout.svg"
-                  alt="logout"
-                  width={24}
-                  height={24}
-                />
-                <p className='text-light-2  max-lg:hidden'>Logout</p>
-              </div>
-            </SignOutButton>
-          </SignedIn>
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2  max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
